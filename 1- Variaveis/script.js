c7ad49d16d195867b517a7674f35e7a0053cb16d
Alphacot <alphacotsystems@gmail.com>
2022-11-02 16:15:58 -0300

    Explicação das Variáveis

---
+++ 1- Variaveis/script.js
@@ -0,0 +1,17 @@
+function VarString (){
+    console.log('Por definição, strings são sequências de caracteres alfanuméricos (letras, números e/ou símbolos) amplamente usadas em programação')
+    console.log('Em Javascript, uma string sempre estará entre aspas. Como por exemplo um nome: "André Luiz" ou um símbolo "@#$%!"')
+}
+
+function VarNumber (){
+    console.log('Number ou número, são sequências de caracteres numéricos (composto por números inteiros ou decimais)')
+    console.log('Em Javascript, um Number normalmente nunca estará entre aspas. Sendo utilizado para outros meios de complementar as STRINGS, como exemplo:')
+    console.log('"Idade: " (string) + 25 (number) "anos" (string) - Perceba que tudo que é number não tem " " entre ele.')
+}
+
+function VarBoolean (){
+    console.log('Variáveis booleanas podem ter o valor Verdadeiro ou Falso, utilizando essas variáveis como operadores lógicos.')
+    console.log('Essas mesmas variáveis podem armazenar o valor de uma expressão de comparação em uma expressão lógica.')
+    console.log('Exemplo de declaração de uma variável do tipo booleano:    var (variável) isDeveloper = true')
+        
+}
