c662f45fa651fd97f292149e365c343229404c89
Alphacot <alphacotsystems@gmail.com>
2022-11-07 16:41:19 -0300

    Números aleatórios + Leia o README

---
+++ 4- Gerar Numero/README.md
@@ -0,0 +1,4 @@
+FEITO O PROPOSTO DE GERAR UM NÚMERO ALEATÓRIO ENTRE 1 A 10.
+ESTÁ COM A NOMENCLATURA: "1D10" no INDEX.HTML
+
+Quis ampliar a TASK com alguns outros geradores de números aleatórios que me lembra D&D.
---
+++ 4- Gerar Numero/script.js
@@ -0,0 +1,24 @@
+function  roll4(){
+    document.getElementById("1d4").innerHTML = Math.floor(Math.random() * 4) + 1;
+}
+
+function  roll6(){
+    document.getElementById("1d6").innerHTML = Math.floor(Math.random() * 6) + 1;
+}
+
+function  roll8(){
+    document.getElementById("1d8").innerHTML = Math.floor(Math.random() * 8) + 1;
+}
+
+function  roll10(){
+    document.getElementById("1d10").innerHTML = Math.floor(Math.random() * 10) + 1;
+
+}
+
+function  roll12(){
+    document.getElementById("1d12").innerHTML = Math.floor(Math.random() * 12) + 1;
+}
+
+function  roll20(){
+    document.getElementById("1d20").innerHTML = Math.floor(Math.random() * 20) + 1;
+}
